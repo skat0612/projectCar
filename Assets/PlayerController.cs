15e8c47a3597eb69b8aae63c542a5258094d5f3f
skat0612 <skat507070@gmail.com>
2024-06-04 11:05:04 +0700

    прикрепил камеру и сделал управление машиной

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    private float horInput;
+    private float verInput;
+    public float turnSpeed = 25;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horInput = Input.GetAxis("Horizontal");
+        verInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * verInput);
+        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horInput);
+    }
+}
